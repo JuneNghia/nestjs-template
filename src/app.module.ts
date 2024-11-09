2f625562928e01f52ec4fdd418d96390ff22159d
junenghia <nguyenminhtrungnghia@gmail.com>
2024-11-09 15:39:11 +0700

    [chore]: authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,36 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MyConfig } from './common/interfaces/my-config.interface';
 
 @Module({
   imports: [
-    PassportModule,
     ConfigModule.forRoot({
       envFilePath: ['.env', '.env.development'],
     }),
-    JwtModule.registerAsync({
+    TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService<MyConfig>) => ({
-        secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: '60s' },
+        type: 'mysql',
+        host: configService.get('DATABASE_HOST'),
+        port: +configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
       }),
     }),
-    // TypeOrmModule.forRootAsync({
-    //   useFactory: (configService: ConfigService<MyConfig>) => ({
-    //     type: 'mysql',
-    //     host: configService.get('DATABASE_HOST'),
-    //     port: +configService.get('DATABASE_PORT'),
-    //     username: configService.get('DATABASE_USER'),
-    //     password: configService.get('DATABASE_PASSWORD'),
-    //     database: configService.get('DATABASE_NAME'),
-    //     entities: [],
-    //     synchronize: true,
-    //   }),
-    // }),
     AuthModule,
   ],
   controllers: [AppController],
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MyConfig } from 'src/common/interfaces/my-config.interface';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<MyConfig>) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '7d' },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
